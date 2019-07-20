0b7a93989f7564854b7e6e840d71dbcc9be43a8d
Mouli110 <52233957+Mouli110@users.noreply.github.com>
2019-07-20 17:04:08 +0530

    Update coin changing minimum.java

--- coin changing minimum.java
+++ coin changing minimum.java
@@ -58,3 +58,55 @@ public static void main (String[] args){
     
 }
 }
+
+
+//0r
+
+
+
+
+
+
+
+import java.util.*;
+
+class   MinCoins{
+    
+    static HashMap<Integer,Integer> h=new HashMap<>();
+    static int minCoins(int total,int coins[]){
+        
+        //if total=0 ,no of coins required =0
+        if(total==0)
+        return 0;
+        
+        // if total is present in hashtable return min coins in hashtable corresponding to total
+        if(h.containsKey(total))
+        return h.get(total);
+        
+        int min=Integer.MAX_VALUE;
+        
+        for(int i=0;i<coins.length;i++){
+            
+            //if coin value is greater than total move on
+            if(coins[i]>total)
+            continue;
+            
+            else
+            min=Math.min(min,1+minCoins(total-coins[i],coins));
+            
+        }
+        
+        h.put(total,min);
+        return min;
+        
+    }
+	public static void main (String[] args) {
+	  
+	  int coins[]={1,2,3};
+	  int total=5;
+	  System.out.println("minimum no of coins "+ minCoins(total,coins));
+	  
+	}
+}
+
+
